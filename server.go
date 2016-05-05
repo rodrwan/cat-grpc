f00c152d0a5c5b3db80364763d1ac26006a98865
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2016-05-05 11:30:07 -0300

    remove url and other packages

--- server.go
+++ server.go
@@ -9,15 +9,12 @@ import (
 	"time"
 
 	"golang.org/x/net/context"
-	"golang.org/x/net/trace"
 
 	"google.golang.org/grpc"
 
 	_ "github.com/lib/pq"
 	pb "github.com/rodrwan/cat-grpc/categoryapi"
 	"github.com/rodrwan/lucky"
-	"google.golang.org/grpc/health"
-	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 type categoryServer struct {
@@ -38,13 +35,12 @@ func (c *categoryServer) Categorize(ctx context.Context, t *pb.Transaction) (*pb
 	return newTrans, nil
 }
 
-func newServer(url, train, labels string) *categoryServer {
+func newServer(train, labels string) *categoryServer {
 	log.Println("Running new server")
 	cs := new(categoryServer)
 	cs.newModel = &lucky.Lucky{
 		LabelsPath:       labels,
 		TrainingDataPath: train,
-		URL:              url,
 	}
 
 	cs.newModel.Fit()
@@ -58,7 +54,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var (
 		port   = flag.Int("port", 10000, "The server port")
-		url    = flag.String("url", "you should set database url", "Database url")
 		path   = flag.String("data", "training_data.txt", "Training data path")
 		labels = flag.String("cats", "labels.txt", "Labels sample path")
 	)
@@ -66,22 +61,13 @@ func main() {
 	log.Println("Running RPC server")
 
 	gs := grpc.NewServer()
-	cs := newServer(*url, *path, *labels)
+	cs := newServer(*path, *labels)
 	pb.RegisterCategoryAPIServer(gs, cs)
-
-	hs := health.NewHealthServer()
-	hs.SetServingStatus("grpc.health.v1.categoryserver", 1)
-	healthpb.RegisterHealthServer(gs, hs)
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	go gs.Serve(lis)
-
-	trace.AuthRequest = func(req *http.Request) (any, sensitive bool) { return true, true }
-	log.Println("Hello service started successfully.")
-	http.HandleFunc("/", handler)
-	log.Fatalln(http.ListenAndServe(":8080", nil))
+	gs.Serve(lis)
 }
